16736600b26bdfae6f566ee953c43d216117b332
GM <guiocm@gmail.com>
2013-04-28 11:30:42 -0700

    two layer Naive Bayes, ~79.7% accuracy

--- nb.py
+++ nb.py
@@ -10,34 +10,38 @@ class NB:
     def __init__(self):
         self.classes = Counter()
         self.features = defaultdict(Counter)
-        self.f_totals = {}
-        self.voc_size = 61499
+        self.f_totals = defaultdict(int)
+        self.vocabulary = set()
+        self.test_samples = 0
+
+    def train_one(self, cl, ftrs):
+        self.classes[cl] += 1
+        self.features[cl].update(ftrs)
+        self.vocabulary.update(ftrs.keys())
+        self.test_samples += 1
+        self.f_totals[cl] += sum(ftrs.values())
     
     def train(self, examples):
         for classes, features in examples:
             for cl in classes:
-                self.classes[cl] += 1
-                self.features[cl].update(features)
+                self.train_one(cl,features)
 
-        self.test_samples = sum(self.classes.values())
-
-        for cl in self.features:
-            self.f_totals[cl] = sum(self.features[cl].values())
+    def classify_one(self, sample):
+        probs = []
+        for cl in self.classes:
+            pr = log(float(self.classes[cl])/self.test_samples)
+            for ftr in sample:
+                pr = pr + log(float(sample[ftr])*\
+                    float(self.features[cl][ftr]+1)/\
+                    (self.f_totals[cl]+len(self.vocabulary)))
+            probs.append((pr, cl))
+        return sorted(probs)[::-1][:3]
 
     def classify(self, samples):
         ret = []
         for sample in samples:
             print sample
-            probs = []
-            for cl in self.classes:
-                pr = log(float(self.classes[cl])/self.test_samples)
-                for ftr in sample:
-                    pr = pr + log(float(sample[ftr])*\
-                        float(self.features[cl][ftr]+1)/\
-                        (self.f_totals[cl]+self.voc_size))
-                probs.append((pr, cl))
-            ret.append(sorted(probs)[::-1][:3])
-
+            ret.append(classify_one(sample))
         return ret
 
 
@@ -56,17 +60,47 @@ def preprocess(data):
     return samples
 
 
-def test(classifier, samples):
-    classes = [el[0] for el in samples]
-    features = [el[1] for el in samples]
+def classtest(samples):
+    t = 0
+    g = 0
+    m = 0
+    w = 0
 
-    output = classifier.classify(features)
-    predicted = [map(lambda x: x[1], el) for el in output]
+    for real, features in samples:
+        cls = [x[0] for x in real]
+        pr = classbayes.classify_one(features)
+        pr = [x[1] for x in pr]
 
+        if cls[0] == pr[0]:
+            t += 1
+        elif pr[0] in cls:
+            g += 1
+        elif cls[0] in pr:
+            m += 1
+        else:
+            w += 1
+
+    print t, g, m, w
+    
+
+def test(samples):
     rights = 0
     wrongs = 0
 
-    for real, pred in zip(classes, predicted):
+    for real, features in samples:
+
+        classpred = classbayes.classify_one(features)
+        output = []
+        for pr, cl in classpred:
+            sub = subclass[cl].classify_one(features)
+            for s_pr, s_cl in sub:
+                output.append((s_pr+pr, s_cl))
+            #output.append(sub[0])
+
+        output = sorted(output)[::-1][:3]
+        pred = [el[1] for el in output]
+
+
         if real[0] == pred[0]:
             print "correct TOP: real - "+str(real)+", pred - "+str(pred)
             rights = rights + 1
@@ -90,16 +124,31 @@ def test(classifier, samples):
 
 
 if __name__ == "__main__":
-    naivebayes = NB()
+    classbayes = NB()
+    subclass = {\
+        "a": NB(),\
+        "b": NB(),\
+        "c": NB(),\
+        "d": NB(),\
+        "e": NB(),\
+        "f": NB(),\
+        "g": NB(),\
+        "h": NB()}
 
     with open(TRAIN, "r") as train_doc:
         train_samples = preprocess(train_doc.readlines())
 
-    naivebayes.train(train_samples)
+    for classes, features in train_samples:
+        cls = set()
+        for cl in classes:
+            if cl[0] not in cls:
+                classbayes.train_one(cl[0], features)
+                cls.add(cl[0])
+            subclass[cl[0]].train_one(cl, features)
 
     with open(TEST, "r") as test_doc:
         test_samples = preprocess(test_doc.readlines())
 
-    test(naivebayes, test_samples)
-
+    test(test_samples)
+    #classtest(test_samples)
 
