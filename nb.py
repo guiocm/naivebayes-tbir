a2cfdeabe36da74a906b777f9ed3e6268ed59d3c
GM <guiocm@gmail.com>
2013-04-28 15:46:07 -0700

    Code now supports classification with one or two classifier levels. Added README.

--- nb.py
+++ nb.py
@@ -1,12 +1,51 @@
-
+import sys
 from collections import defaultdict, Counter
 from math import log
 
-TRAIN = "wipoalpha-train.txt"
-TEST = "wipoalpha-test.txt"
+class Classifier:
+    def __init__(self):
+        pass
+
+    def test(self, samples):
+        rights = 0
+        wrongs = 0
+
+        for real, features in samples:
+
+            output = self.classify_one(features)
+            pred = [el[1] for el in output]
+
+            if real[0] == pred[0]:
+                if VERBOSE:
+                    print "correct TOP: real - "+str(real)+\
+                        ", pred - "+str(pred)
+                rights = rights + 1
+            elif real[0] in pred:
+                if VERBOSE:
+                    print "correct GSS: real - "+str(real)+\
+                        ", pred - "+str(pred)
+                rights = rights + 1
+            elif pred[0] in real:
+                if VERBOSE: 
+                    print "correct ALL: real - "+str(real)+\
+                        ", pred - "+str(pred)
+                rights = rights + 1
+            else:
+                if VERBOSE:
+                    print "WRONG: real - "+str(real)+", pred - "+str(pred)
+                wrongs = wrongs + 1
+
+        if VERBOSE:
+            print ""
+            print "finished"
+        print "correct: " + str(rights)
+        print "wrong: " + str(wrongs)
 
 
-class NB:
+#
+# Implements a simple Multinomial Naive Bayes classifier
+#
+class NB(Classifier):
     def __init__(self):
         self.classes = Counter()
         self.features = defaultdict(Counter)
@@ -31,8 +70,7 @@ class NB:
         for cl in self.classes:
             pr = log(float(self.classes[cl])/self.test_samples)
             for ftr in sample:
-                pr = pr + log(float(sample[ftr])*\
-                    float(self.features[cl][ftr]+1)/\
+                pr = pr + log(float(self.features[cl][ftr]+1)/\
                     (self.f_totals[cl]+len(self.vocabulary)))
             probs.append((pr, cl))
         return sorted(probs)[::-1][:3]
@@ -45,6 +83,10 @@ class NB:
         return ret
 
 
+#
+# Preprocesses the input data, removing the lowest level of classification, 
+# and returning tuples of a list of classes and the feature dictionary.
+#
 def preprocess(data):
     samples = []
     for line in data:
@@ -83,72 +125,64 @@ def classtest(samples):
     print t, g, m, w
     
 
-def test(samples):
-    rights = 0
-    wrongs = 0
+class TwolevelNB(Classifier):
+    def __init__(self):
+        self.classbayes = NB()
+        self.subclass = {\
+            "a": NB(),\
+            "b": NB(),\
+            "c": NB(),\
+            "d": NB(),\
+            "e": NB(),\
+            "f": NB(),\
+            "g": NB(),\
+            "h": NB()}
 
-    for real, features in samples:
+    def train(self, examples):
+        for classes, features in examples:
+            cls = set()
+            for cl in classes:
+                if cl[0] not in cls:
+                    self.classbayes.train_one(cl[0], features)
+                    cls.add(cl[0])
+                self.subclass[cl[0]].train_one(cl, features)
 
-        classpred = classbayes.classify_one(features)
+    def classify_one(self, sample):
+        classpred = self.classbayes.classify_one(sample)
         output = []
         for pr, cl in classpred:
-            sub = subclass[cl].classify_one(features)
+            sub = self.subclass[cl].classify_one(sample)
             for s_pr, s_cl in sub:
                 output.append((s_pr+pr, s_cl))
-            #output.append(sub[0])
 
-        output = sorted(output)[::-1][:3]
-        pred = [el[1] for el in output]
-
-
-        if real[0] == pred[0]:
-            print "correct TOP: real - "+str(real)+", pred - "+str(pred)
-            rights = rights + 1
-        elif real[0] in pred:
-            print "correct GSS: real - "+str(real)+", pred - "+str(pred)
-            rights = rights + 1
-        elif pred[0] in real:
-            print "correct ALL: real - "+str(real)+", pred - "+str(pred)
-            rights = rights + 1
-        else:
-            print "WRONG: real - "+str(real)+", pred - "+str(pred)
-            wrongs = wrongs + 1
-
-    print ""
-    print "finished"
-    print "correct: " + str(rights)
-    print "wrong: " + str(wrongs)
-
-
-
+        return sorted(output)[::-1][:3]
 
 
 if __name__ == "__main__":
-    classbayes = NB()
-    subclass = {\
-        "a": NB(),\
-        "b": NB(),\
-        "c": NB(),\
-        "d": NB(),\
-        "e": NB(),\
-        "f": NB(),\
-        "g": NB(),\
-        "h": NB()}
+    LEVELS = "1"
+    TRAIN = "wipoalpha-train.txt"
+    TEST = "wipoalpha-test.txt"
+    VERBOSE = False
+
+    if len(sys.argv) > 1:
+        LEVELS = sys.argv[1]
+
+    if len(sys.argv) > 3:
+        TRAIN = sys.argv[2]
+        TEST = sys.argv[3]
+
+    if len(sys.argv) == 5 and sys.argv[4] == "verbose":
+        VERBOSE = True
+        
+    classifier = NB() if LEVELS == "1" else TwolevelNB()
 
     with open(TRAIN, "r") as train_doc:
         train_samples = preprocess(train_doc.readlines())
 
-    for classes, features in train_samples:
-        cls = set()
-        for cl in classes:
-            if cl[0] not in cls:
-                classbayes.train_one(cl[0], features)
-                cls.add(cl[0])
-            subclass[cl[0]].train_one(cl, features)
+    classifier.train(train_samples)
 
     with open(TEST, "r") as test_doc:
         test_samples = preprocess(test_doc.readlines())
 
-    test(test_samples)
-    #classtest(test_samples)
+    classifier.test(test_samples)
 
